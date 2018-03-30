b39ff75312d48c44082199f7039d057e2f26f067
Unknown <zachary.demaria@gmail.com>
2018-03-30 17:46:57 +1030

    actually staged changes
    oops

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BallMovement : MonoBehaviour {
+    public float speed = 5;
+    public float velocityKeep = 0.1f;
+    Rigidbody rb;
+
+
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.velocity = transform.forward * speed;
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Wall":
+                transform.forward = Vector3.Reflect(transform.forward, collision.contacts[0].normal) + ((collision.rigidbody != null) ? Vector3.right * velocityKeep * collision.rigidbody.velocity.x : Vector3.zero);
+                break;
+            default:
+
+                break;
+        }
+
+    }
+}
---
+++ Assets/Scripts/PaddleMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PaddleMovement : MonoBehaviour {
+    public float moveSpeed = 4;
+    public float distanceConstraint = 3.5f;
+    public float timeFadeFactor = 0.3f;
+    Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizIn = Input.GetAxisRaw("Horizontal");
+        Time.timeScale = Mathf.Lerp(Time.timeScale, Mathf.Abs(horizIn), Time.unscaledDeltaTime * timeFadeFactor);
+        Vector3 posOffset = new Vector3(Mathf.Clamp(transform.position.x + Time.deltaTime * moveSpeed * horizIn, -distanceConstraint, distanceConstraint), transform.position.y, transform.position.z);
+        rb.velocity = posOffset - transform.position;
+        rb.MovePosition(posOffset);
+        print(rb.velocity);        
+	}
+}
